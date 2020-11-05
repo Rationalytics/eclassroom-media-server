d68e004f9bf7a876759399ead2a44f951ac9a071
Prasad Sawant <prasadsawant5@gmail.com>
2020-11-05 17:31:52 +0530

    env variables prepared

--- config/ci.js
+++ config/ci.js
@@ -1,8 +1,5 @@
 module.exports = {
     secret: ']gQcZZmM4?xr#zNhK.!GB$7av_8B(L',
-    sendinBlueApiKey: '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',
-    fast2SmsApiKey: 'm2Hwu8MDgN0OdFtBoy5G67IQ34YlXkRAVKqearLnsfWiSvxbTCk4d63GXWsNh1o8aemBDuOizIq7jSCF',
-    urlShortnerApi: '6c176f94d0654297608527d30ad13919559f2',
     operationsServer: 'operations.meraclass.in:3100',
     clientUrl: 'http://localhost:4200',
     openViduUrl: 'https://localhost:4443/',
--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,5 @@
 module.exports = {
     secret: 'MiKb_VppNN9$&u,L3]86DXwGTPpm$8',
-    sendinBlueApiKey: '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',
-    fast2SmsApiKey: 'm2Hwu8MDgN0OdFtBoy5G67IQ34YlXkRAVKqearLnsfWiSvxbTCk4d63GXWsNh1o8aemBDuOizIq7jSCF',
-    urlShortnerApi: '6c176f94d0654297608527d30ad13919559f2',
     operationsServer: 'localhost:3100',
     clientUrl: 'http://localhost:4200',
     openViduUrl: 'https://localhost:4443/',
